854a3617d8d7b54398b575ab5c15843b3028baee
Augustinetharakan12 <augustinetharakan12@gmail.com>
2017-09-16 23:41:03 +0530

    forms updated

--- osat/models.py
+++ osat/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 class alumni(models.Model):
-    fname=models.CharField(max_length=200)
-    lname=models.CharField(max_length=200)
-    year_pass=models.IntegerField(max_length=5)
+    fname=models.CharField(max_length=200,null=True)
+    lname=models.CharField(max_length=200,null=True)
+    year_pass=models.IntegerField(null=True)
     id = models.IntegerField(primary_key=True)
-    student=models.CharField(max_length=2)#y or N
+    student=models.CharField(max_length=2,null=True)#y or N
     inst_name=models.CharField(max_length=200,null=True)
-    phno=models.CharField(max_length=15)
-    email=models.CharField(max_length=100)
+    phno=models.CharField(max_length=15,null=True)
+    email=models.CharField(max_length=100,null=True)
     #dob=models.DateField
     # address info
     a_street = models.CharField(max_length=200,null=True)
     a_city = models.CharField(max_length=200,null=True)
     a_state = models.CharField(max_length=200,null=True)
     a_country = models.CharField(max_length=200,null=True)
-    a_pin = models.CharField(max_length=50)
+    a_pin = models.CharField(max_length=50,null=True)
     def __str__(self):
         return self.fname+' '+ self.lname
 
