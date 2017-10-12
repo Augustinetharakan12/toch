91dd5716bf8e237b1b5300d29ce981ea80dee218
augustine <augustinetharakan12@gmail.com>
2017-10-12 23:07:49 +0530

    notif

--- osat/views.py
+++ osat/views.py
@@ -1,11 +1,12 @@
+import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from . forms import *
 from . models import *
 def index(request):
-    cont_dict={'notif' : notif.objects.all().order_by('-timestamp')}
+    dn=datetime.datetime.now()
+    cont_dict={'notif' : notif.objects.all().order_by('-timestamp'),'dn':dn}
     return render(request, "osat/index.html", cont_dict)
-
 
 
 def about(request):
