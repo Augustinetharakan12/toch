9849f89535c72a06dc5999974428254e2207440c
kurian <github719@gmail.com>
2017-10-12 19:05:40 +0530

    all of today

--- osat/static/osat/script.js
+++ osat/static/osat/script.js
@@ -1,4 +1,28 @@
 
-	$(document).ready(function(){
-$('#logo-modal').modal('show');
-	});
+$(document).ready(function(){
+    if (readCookie('osat-visit') == null) {
+        $('#logo-modal').modal('show');
+        createCookie('osat-visit', '', 0.001);
+    }
+});
+
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
